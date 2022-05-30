28d68a0650f0841f424637114dd76ebe20465898
carlos2049 <carlos.20499@gmail.com>
2022-05-30 18:53:42 -0400

    final fundamentals angular

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Product } from './product.model'
 
 @Component({
   selector: 'app-root',
@@ -6,6 +7,7 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
+  widthImg = 10
   name = 'carloss';
   age = 30
   img = 'https://www.fundacion-affinity.org/sites/default/files/los-10-sonidos-principales-del-perro.jpg'
@@ -16,8 +18,53 @@ export class AppComponent {
     avatar: 'https://www.fundacion-affinity.org/sites/default/files/los-10-sonidos-principales-del-perro.jpg'
   }
 
+  register = {
+    name: '',
+    email: '',
+    password: ''
+  }
+
   names: string[] = ['carlos', 'esteban', 'marco']
   newName = ''
+  box = {
+    width: 100,
+    height: 100,
+    background: 'red'
+  }
+  products: Product[] = [
+    {
+      name: 'EL mejor juguete',
+      price: 565,
+      image: './assets/images/toy.jpg',
+      category: 'all',
+    },
+    {
+      name: 'Bicicleta casi nueva',
+      price: 356,
+      image: './assets/images/bike.jpg'
+    },
+    {
+      name: 'Colleción de albumnes',
+      price: 34,
+      image: './assets/images/album.jpg'
+    },
+    {
+      name: 'Mis libros',
+      price: 23,
+      image: './assets/images/books.jpg'
+    },
+    {
+      name: 'Casa para perro',
+      price: 34,
+      image: './assets/images/house.jpg'
+    },
+    {
+      name: 'Gafas',
+      price: 3434,
+      image: './assets/images/glasses.jpg'
+    }
+  ]
+
 
   toggleButton() {
     this.btnDisabled = !this.btnDisabled
@@ -42,6 +89,10 @@ export class AppComponent {
   }
   deleteName(i: number) {
     this.names.splice(i, 1)
+  }
+  onRegister() {
+    console.log(this.register);
+
   }
 
 }
